8f85aa05556f669f8944a1b69d2dbc7c4edce6fa
07ronak <ronakhingonia123456789@gmail.com>
2024-07-13 00:09:01 +0530

    path change in app.js ans show.ejs

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
@@ -83,6 +83,10 @@ app.use((req, res, next) => {
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   next();
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/listing");
 });
 
 app.use("/listing", listingRouter);
